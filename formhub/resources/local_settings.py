ba6f757d217eeafaed1c28b754146b3416776cbd
Larry Weya <larryweya@gmail.com>
2013-09-20 08:27:13 +0000

    Renamed formhub to formhub base and formhub deploy to formhub

---
+++ formhub/resources/local_settings.py
@@ -0,0 +1,51 @@
+import os
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ['*']
+# mysql
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.environ['MYSQL_HOST'],
+        'NAME': 'formhub',
+        'USER': 'formhub',
+        'PASSWORD': 'watermelon',
+    }
+}
+
+# postgres
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'formhub_dev',
+#        'USER': 'formhub_dev',
+#        'PASSWORD': '',
+#    }
+#}
+
+# sqlite
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'db.sqlite3',
+#    }
+#}
+
+TOUCHFORMS_URL = 'http://localhost:9000/'
+
+MONGO_DATABASE = {
+    'HOST': os.environ['MONGO_HOST'],
+    'PORT': 27017,
+    'NAME': 'formhub',
+    'USER': '',
+    'PASSWORD': ''
+}
+BROKER_URL='amqp://guest:guest@{RABBITMQ_HOST}:5672'.format(RABBITMQ_HOST=os.environ['RABBITMQ_HOST'])
+MEDIA_URL='/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ['SMTP_HOST']
+ADMINS = ('info@joomla-limited.com',)
+SERVER_EMAIL = 'noreply@docker.ona.io'
+DEFAULT_FROM_EMAIL = 'postmaster@docker.ona.io'
